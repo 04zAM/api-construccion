878002841f519048a868a740df40b0c806923b08
Esteban Vasquez <49489566+04zAM@users.noreply.github.com>
2022-01-03 11:35:41 -0500

    hide password

--- src/cnn.js
+++ src/cnn.js
@@ -7,7 +7,7 @@ const config = {
   port: "5432",
   database: "ddpenflqbattk5",
   user: "hjizsgtokyjdfa",
-  password: "109e41d95f27477210d76c30e830b2e7687518a28c5e7955bed4414d5a743c23",
+  password: "password",
   ssl: ssl,
 };
 
