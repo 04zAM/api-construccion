6833eae827d41c7eca7ba4a75b1514d8484f54ef
Esteban Vasquez <49489566+04zAM@users.noreply.github.com>
2022-01-17 16:11:45 -0500

    hide db credentials

--- src/cnn.js
+++ src/cnn.js
@@ -6,8 +6,8 @@ const config = {
   host: "ec2-34-224-239-147.compute-1.amazonaws.com",
   port: "5432",
   database: "ddpenflqbattk5",
-  user: "hjizsgtokyjdfa",
-  password: "109e41d95f27477210d76c30e830b2e7687518a28c5e7955bed4414d5a743c23",
+  user: "user",
+  password: "pasword",
   ssl: ssl,
 };
 
